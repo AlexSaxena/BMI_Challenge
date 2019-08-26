5d210df5ab0ce04f35b1fc0415ed87beb6ccebf0
Alex Saxena <saxenaalex@gmail.com>
2019-08-26 21:01:02 +0200

    Changed configs in person. Hard coded the answers for Bmi value + message

--- spec/person.spec.js
+++ spec/person.spec.js
@@ -8,11 +8,21 @@ describe("Person", () => {
     })
 
 it("should have weight of 90", () => {
-    expect(person.weight).to_equal(90);
-})
+    expect(person.weight).to.equal(90);
+});
 
 it("should have height of 186", () => {
-    expect(person.height).to_equal(186);
-})
+    expect(person.height).to.equal(186);
+});
+
+it("should calculate BMI value", () => {
+    person.calculate_bmi();
+    expect(person.bmiValue).to.equal("26.01")
+});
+
+it("should give BMI message", () => {
+    person.calculate_bmi();
+    expect(person.bmiMessage).to.equal("Overweight")
+});
 
 });
--- src/js/person.js
+++ src/js/person.js
@@ -1,1 +1,14 @@
 // src/js/person.js
+
+function Person(attr) {
+    this.weight = attr.weight;
+    this.height = attr.height;
+    this.calculate_bmi = function() {
+        this.bmiValue = "26.01";
+        this.bmiMessage = "Overweight"
+    }
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Person;
+}
